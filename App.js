1986bca251b34c27234ce3eaae1d8136bad78d23
humakl <st.sinnerman@gmail.com>
2021-07-03 21:26:54 +0400

    first version of app

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList, Alert, TextInput } from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+import nextId from 'react-id-generator';
+
+const App = () => {
+
+  const [items, setItems] = useState([]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert("Error", "Please enter an item", [{ text: "OK" }]);
+    } else {
+      setItems((prevItems) => {
+        return [{ id: nextId(), text }, ...prevItems];
+      });
+    };
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
